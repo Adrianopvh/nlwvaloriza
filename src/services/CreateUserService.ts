8174ead6a26e5cc52a4f18d6d7258c54096ee48a
Adrianopvh <adriano.rodriques12389@gmail.com>
2021-06-27 11:31:25 -0400

    Criando Criptografia do password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,30 +1,44 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/repositories";
 import { AppError } from "../shared/errors/AppError";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
-  name: string;
-  email: string;
-  admin?: boolean;
+ name: string;
+ email: string;
+ admin?: boolean;
+ password: string;
 }
+
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
-    const usersRepository = getCustomRepository(UsersRepositories);
-    if(!email) {
-      throw new AppError("Email incorrect");
-    }
-    const userAlreadyExists = await usersRepository.findOne({
-      email,
-    });
-    if (userAlreadyExists) {
-      throw new AppError("User already exists");
-    }
-    const user = usersRepository.create({
-     name, email, admin,
-    });
-    await usersRepository.save(user);
-    return user;
+ async execute({ name, email, admin, password }: IUserRequest) {
+  const usersRepository = getCustomRepository(UsersRepositories);
+
+  if (!email) {
+   throw new AppError("Email incorrect");
   }
+
+  const userAlreadyExists = await usersRepository.findOne({
+   email,
+  });
+
+  if (userAlreadyExists) {
+   throw new AppError("User already exists");
+  }
+
+  const passwordHash = await hash(password, 8)
+
+  const user = usersRepository.create({
+   name, 
+   email, 
+   admin, 
+   password: passwordHash,
+  });
+
+  await usersRepository.save(user);
+
+  return user;
+ }
 }
 
 export { CreateUserService };
